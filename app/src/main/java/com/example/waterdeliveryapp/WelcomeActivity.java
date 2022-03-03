40dad1e583f115535e34549013bbddaa2557842a
pradeep <mdeep1992@gmail.com>
2022-03-03 09:52:06 +0530

    connect to firebase

--- app/src/main/java/com/example/waterdeliveryapp/WelcomeActivity.java
+++ app/src/main/java/com/example/waterdeliveryapp/WelcomeActivity.java
@@ -3,7 +3,15 @@ package com.example.waterdeliveryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WelcomeActivity extends AppCompatActivity {
 Button btnSignIn,btnSignUp;
@@ -11,10 +19,42 @@ Button btnSignIn,btnSignUp;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("MY DATA");
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d("Water_delivery_app" ,"Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("Water_delivery_app", "Failed to read value.", error.toException());
+            }
+        });
+
         btnSignIn=findViewById(R.id.btn_signin);
         btnSignUp=findViewById(R.id.btn_signup);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+            }
 
+        });
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
+            }
+        });
 
     }
 }
