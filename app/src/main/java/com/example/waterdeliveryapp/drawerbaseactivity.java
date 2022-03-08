dff30ce1876dd487f233c8db6adc6acf3627118b
pradeep <mdeep1992@gmail.com>
2022-03-08 20:22:05 +0530

    dashboardactivity,drawerbase activity,contentlayout,drawerheader,mainmenu..all UIdesign completed

---
+++ app/src/main/java/com/example/waterdeliveryapp/drawerbaseactivity.java
@@ -0,0 +1,14 @@
+package com.example.waterdeliveryapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class drawerbaseactivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawerbaseactivity);
+    }
+}
